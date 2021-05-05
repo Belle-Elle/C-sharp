e170bfbbcfe573a1a495d215b2080cf01b26e07b
Isabelle <isa2004.medeirosamancio@gmail.com>
2021-05-05 12:08:19 -0300

    Exemplo Console

---
+++ Console das notas e medias/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Console_das_notas_e_medias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercicio Da média");
+            
+            Console.WriteLine("Qual a primeira nota?");
+            Double primeiranota = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual a segunda nota?");
+            Double segundanota = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual a terceira nota?");
+            Double terceiranota = Double.Parse(Console.ReadLine());
+
+            Double media = (primeiranota + segundanota + terceiranota) / 3;
+            Console.WriteLine("A media foi de: " + media.ToString("N1"));
+
+            if(media >5)
+            {
+            //    Se a media do aluno for maior que 5 ele é aprovado
+            Console.WriteLine("O Aluno foi aprovado!!Congratulations!");
+            } else {
+
+            //    se a media do aluno for menor que 5 ele é reprovado
+            Console.WriteLine("O Aluno foi reprovado Sinto muito");
+            }
+
+        }
+    }
+}
---
+++ exemplo console/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace exemplo_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Qual seu nome?");
+           string nomePessoa = Console.ReadLine();
+
+           Console.WriteLine("Saldações Humano " + nomePessoa + "!!");
+
+            Console.WriteLine("Qual é o primeiro numero?");
+            // Qual o estado do semaforo
+            int primeironumero = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Qual é o segundo numero?");
+            int segundonumero = int.Parse(Console.ReadLine());
+
+            int soma = primeironumero +  segundonumero;
+
+            Console.WriteLine("A soma dos numeros foi: " + soma);
+        }
+    }
+}
